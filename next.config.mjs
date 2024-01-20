51566c3288538b0c1c8cbcfa88a8fd56218e7b66
ChuanXinNg <ngcx0103@gmail.com>
2024-01-20 21:23:17 +0800

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWAConfig = withPWAInit({
+  pwa: {
+    dest: 'public',
+    // other PWA options go here
+  },
+  env: {
+    metadataBase: 'http://localhost:3000', // Set your actual app URL here
+  },
+  // other Next.js configurations...
+});
+
+// Export the configuration object returned by withPWAInit
+export default withPWAConfig;

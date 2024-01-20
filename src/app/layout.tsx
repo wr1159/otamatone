ef9620f3b15c4c6ae067968950564acb362a2a53
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-01-20 18:25:28 +0800

    Disable Scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import FrequencySlider from "@/components/frequency-bar";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <h1>Audio Player App</h1>
-      <FrequencySlider onChange={function (frequency: number): void {}} />
+    <main className="fixed">
+      <div className="flex screen flex-col items-center justify-between">
+        <Navbar />
+        <h1>Audio Player App</h1>
+        <FrequencySlider onChange={function (frequency: number): void {}} />
+      </div>
     </main>
   );
 }

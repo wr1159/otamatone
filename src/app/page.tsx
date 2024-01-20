4856833378b71fe88401a8cca8694a27fac4a980
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-01-20 18:02:07 +0800

    Remove min h-screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import FrequencySlider from "@/components/frequency-bar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <h1>Audio Player App</h1>
       <FrequencySlider onChange={function (frequency: number): void {}} />
     </main>

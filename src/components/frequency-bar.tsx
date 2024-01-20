e028320d69be715828e8f6ed3a722f886fb8050a
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-01-20 17:16:18 +0800

    Fix sound

--- src/components/frequency-bar.tsx
+++ src/components/frequency-bar.tsx
@@ -1,48 +1,55 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import FrequencyPlayer from './frequency-player';
+"use client";
+import React, { useState, useEffect } from "react";
 
 interface FrequencySliderProps {
-  onChange: (frequency: number) => void;
+  onChange?: (frequency: number) => void;
 }
 
 const FrequencySlider: React.FC<FrequencySliderProps> = ({ onChange }) => {
   const [frequency, setFrequency] = useState(440); // Default frequency
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [oscillator, setOscillator] = useState<OscillatorNode | null>(null);
 
-  useEffect(() => {
-    // Trigger the parent component's onChange callback when the frequency changes
-    setFrequency(frequency);
-    stopSound()
-    startSound()
-  }, [frequency, onChange]);
-  
-  
-  const handleFrequencyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFrequencyChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const newFrequency = parseInt(event.target.value, 10);
     setFrequency(newFrequency);
   };
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [oscillator, setOscillator] = useState<OscillatorNode | null>(null);
-  const context = new (window.AudioContext || (window as any).webkitAudioContext)();
-  const o = context.createOscillator();
-  const g = context.createGain();
-  o.start(0);
+
+  useEffect(() => {
+    // Trigger the parent component's onChange callback when the frequency changes
+    console.log("oscillator", oscillator);
+    if (oscillator) {
+      oscillator.frequency.value = frequency;
+    }
+  }, [frequency]);
+
+  useEffect(() => {
+    if (isPlaying && !oscillator) {
+      startSound();
+    } else {
+      stopSound();
+    }
+  }, [isPlaying]);
 
   const startSound = () => {
-  
+    const context = new (window.AudioContext ||
+      (window as any).webkitAudioContext)();
+    const o = context.createOscillator();
+    const g = context.createGain();
+    o.start(0);
     o.frequency.value = frequency;
     o.connect(g);
     g.connect(context.destination);
-    
-    g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 5)
-    setIsPlaying(true);
-    setOscillator(o);
+    if (oscillator == null) {
+      setOscillator(o);
+    }
   };
 
   const stopSound = () => {
     if (oscillator) {
       oscillator.stop();
-      setIsPlaying(false);
       setOscillator(null);
     }
   };
@@ -58,13 +65,13 @@ const FrequencySlider: React.FC<FrequencySliderProps> = ({ onChange }) => {
         step="1"
         value={frequency}
         onChange={handleFrequencyChange}
-        style={{ transform: 'rotate(270deg)' }}
+        style={{ transform: "rotate(270deg)" }}
       />
-    <div>
-      <button onClick={isPlaying ? stopSound : startSound}>
-        {isPlaying ? 'Stop Sound' : 'Play Sound'}
-      </button>
-    </div>
+      <div>
+        <button onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? "Stop Sound" : "Play Sound"}
+        </button>
+      </div>
     </div>
   );
 };
